4ea01630c46bfcfcf1563cf6154345d7cfe2f616
Giuseppe Miragliotta <giuseppe.miragliotta@xtreamers.io>
2023-05-03 15:30:42 +0200

    feat: add basic and premium examples (#1)
    * feat: prepare new codebase
    
    * feat: add basic coffee machine
    
    * feat: add cappuccino machine
    
    * feat: parameterize coffee and cappuccino machine run
    
    * refactor: drop CoffeeMachine interface

--- coffee-machine/bad/basic/src/Main.java
+++ coffee-machine/bad/basic/src/Main.java
@@ -1,66 +1,43 @@
+package basic;
+
 import java.util.Scanner;
-
-enum CoffeeCommand {
-    COFFEE,
-    CAPPUCCINO,
-    ADD_WATER,
-    ADD_BEANS,
-    ADD_MILK,
-
-    INSERT_COIN,
-    EXIT
-}
 
 public class Main {
     public static void main(String[] args) {
-        CoffeeMachine coffeeMachine = new CoffeeMachine(500, 100, 200, 200);
+        CoffeeMachine coffeeMachine = new CoffeeMachine(500, 100,  200);
 
         Scanner scanner = new Scanner(System.in);
         CoffeeCommand input;
 
         while (true) {
-            System.out.println("Enter command (coffee, cappuccino, add_water, add_beans, add_milk, insert_coin, exit): ");
+            System.out.println("Enter command (coffee, add_water, add_beans, insert_coin, exit): ");
             try {
-
                 input = CoffeeCommand.valueOf(scanner.nextLine().toUpperCase());
 
                 switch (input) {
-                    case COFFEE -> coffeeMachine.orderDrink(CoffeeMachine.DrinkType.COFFEE);
-                    case CAPPUCCINO -> {
-                        try {
-                            coffeeMachine.orderDrink(CoffeeMachine.DrinkType.CAPPUCCINO);
-                        } catch (UnsupportedOperationException e) {
-                            System.out.println("CoffeeMachine can't make cappuccino.");
-                        }
-                    }
+                    case COFFEE -> coffeeMachine.makeCoffee();
+
                     case ADD_WATER -> {
                         System.out.println("Enter the amount of water to add: ");
                         int water = scanner.nextInt();
-                        scanner.nextLine(); // Consume newline character
+                        scanner.nextLine();
                         coffeeMachine.addWater(water);
                         System.out.println("Water added.");
                     }
                     case ADD_BEANS -> {
                         System.out.println("Enter the amount of coffee beans to add: ");
                         int beans = scanner.nextInt();
-                        scanner.nextLine(); // Consume newline character
+                        scanner.nextLine();
                         coffeeMachine.addCoffeeBeans(beans);
                         System.out.println("Coffee beans added.");
-                    }
-                    case ADD_MILK -> {
-                        System.out.println("Enter the amount of milk to add: ");
-                        int milk = scanner.nextInt();
-                        scanner.nextLine(); // Consume newline character
-                        coffeeMachine.addMilk(milk);
-                        System.out.println("Milk added.");
                     }
                     case INSERT_COIN -> {
                         System.out.print("Enter a coin: ");
                         long amount = scanner.nextLong();
                         coffeeMachine.insertCoins(amount);
-                        scanner.nextLine(); // Consume newline character
+                        scanner.nextLine();
                         System.out.println("Inserted " + amount + " cents");
-                        System.out.println("Your credit is now " + coffeeMachine.userCredit + " cents");
+                        System.out.println("Your credit is now " + coffeeMachine.getUserCredit() + " cents");
                     }
                     case EXIT -> {
                         scanner.close();
@@ -68,7 +45,6 @@ public class Main {
                     }
                 }
             } catch (Exception e) {
-                // scanner.close();
                 System.out.println("Invalid command.");
             }
 
---
+++ coffee-machine/bad/premium/CappuccinoMachine.java
@@ -0,0 +1,51 @@
+package premium;
+
+
+class CappuccinoMachine extends CoffeeMachine {
+    int milkLevel;
+    CappuccinoMachine(int waterLevel, int coffeeBeansLevel, int milkLevel) {
+        super(waterLevel, coffeeBeansLevel, 0);
+        this.milkLevel = milkLevel;
+    }
+
+    @Override
+    protected long getCoffeeCost() {
+        return 0;
+    }
+
+    @Override
+    protected boolean canMakeDrink() {
+        return waterLevel >= 100 && coffeeBeansLevel >= 20 && milkLevel >= 50;
+    }
+
+    @Override
+    public void insertCoins(long amount) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public long getUserCredit() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected boolean creditIsSufficient() {
+        return true;
+    }
+
+    public void addMilk(int milk) {
+        this.milkLevel += milk;
+    }
+
+    public void makeCappuccino() {
+        if (canMakeDrink()) {
+            waterLevel -= 100;
+            coffeeBeansLevel -= 20;
+            milkLevel -= 50;
+            System.out.println("Cappuccino is ready!");
+        } else {
+            System.out.println("Not enough ingredients for cappuccino!");
+        }
+    }
+
+}
